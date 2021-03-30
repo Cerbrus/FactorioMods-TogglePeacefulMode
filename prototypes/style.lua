320195e3090b99f1bc8b2f7eb58ef302a0eac155
Lexxy Fox <lexxyfox@gmail.com>
2021-03-30 10:49:04 -0700

    Migration to mod-gui

--- prototypes/style.lua
+++ prototypes/style.lua
@@ -1,33 +1,16 @@
-data.raw["gui-style"].default["tpm_button"] = {
-  type = "button_style",
-  parent = "button",
-  width = 32,
-  height = 32,
-  top_padding = 0,
-  right_padding = 0,
-  bottom_padding = 0,
-  left_padding = 0,
-  left_click_sound = {{
-    filename = "__core__/sound/gui-click.ogg",
-    volume = 1
-  }}
-}
-
 data:extend({
   {
     type = "sprite",
     name = "tpm_button_sprite_peace",
     filename = "__TogglePeacefulMode__/graphics/peace.png",
     priority = "extra-high-no-scale",
-    width = 32,
-    height = 32,
+    size = {32, 32},
   },
   {
     type = "sprite",
     name = "tpm_button_sprite_war",
     filename = "__TogglePeacefulMode__/graphics/war.png",
     priority = "extra-high-no-scale",
-    width = 32,
-    height = 32,
+    size = {32, 32},
   }
 });
