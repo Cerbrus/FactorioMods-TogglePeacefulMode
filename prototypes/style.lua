c9b92103217bb8b729ec7d5dd0595bcda96f4e3f
Lexxy Fox <lexxyfox@gmail.com>
2021-03-30 11:18:19 -0700

    Removed unused graphics; deduplicated and refactored used graphics.

--- prototypes/style.lua
+++ prototypes/style.lua
@@ -2,14 +2,14 @@ data:extend({
   {
     type = "sprite",
     name = "tpm_button_sprite_peace",
-    filename = "__TogglePeacefulMode__/graphics/peace.png",
+    filename = "__TogglePeacefulMode__/thumbnail.png",
     priority = "extra-high-no-scale",
     size = {32, 32},
   },
   {
     type = "sprite",
     name = "tpm_button_sprite_war",
-    filename = "__TogglePeacefulMode__/graphics/war.png",
+    filename = "__TogglePeacefulMode__/war.png",
     priority = "extra-high-no-scale",
     size = {32, 32},
   }
