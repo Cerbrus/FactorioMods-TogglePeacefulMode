b3804aa961ea593168333a20cc39aae2de179e86
Lexxy Fox <lexxyfox@gmail.com>
2021-03-30 21:11:34 -0700

    Migrated to using SVGs.

--- data.lua
+++ data.lua
@@ -3,12 +3,12 @@ data:extend{
     type = "sprite",
     name = "tpm_button_sprite_peace",
     filename = "__TogglePeacefulMode__/thumbnail.png",
-    size = {32, 32},
+    size = {64, 64},
   },
   {
     type = "sprite",
     name = "tpm_button_sprite_war",
     filename = "__TogglePeacefulMode__/war.png",
-    size = {32, 32},
+    size = {64, 64},
   }
 };
